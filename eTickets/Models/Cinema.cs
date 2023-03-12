c97d033a8ee19484f244251d5e013c7b2096773e
HaBao2907 <hagiabao8461@gmail.com>
2023-03-12 16:58:51 +0700

    git commitall

---
+++ eTickets/Models/Cinema.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+namespace eTickets.Models
+{
+    public class Cinema
+    {
+        [Key]
+        public int CinemaId { get; set; }
+        public string Logo { get; set; }
+        public string Name { get; set; }
+        public string Description {get; set;}
+
+    }
+}
---
+++ eTickets/Models/Producer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+namespace eTickets.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int ProducerId { get; set; }
+        public string ProfilePicture { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+    }
+}
